d26b5d4840b16104775d41fd9ef9af45be691ef2
Luis Alejandro <alfaroalejandro597@gmail.com>
2021-06-09 11:40:36 -0600

    miercoles trabajo

--- hang_man_game.py
+++ hang_man_game.py
@@ -1,14 +1,35 @@
 import os
+import random
 import ascii_magic
 
 
+# Esta funcion guarda las palabras de data en una lista y ejecuta las funcionalidades del juego
 def read():
-    palabra = ""
+    palabras = []
+
     with open("./files/data.txt", "r", encoding="utf-8") as f:
-        pass
+        for line in f:
+            palabras.append(line)
+        palabra_escogida = random.choice(palabras)
+
+    for size in palabra_escogida:
+        print("_ ", end='')
+    print("")
+
+    letra = input("")
 
 
 
+
+
+
+
+
+
+
+
+
+# esta funcion se encarga de las funcionalidades basicas del menu
 def main():
 
     while True:
@@ -25,11 +46,14 @@ def main():
             output = ascii_magic.from_image_file("./image/calabaza.jpg", columns=100, char="@")
             ascii_magic.to_terminal(output)
 
-            print("Bienvenido....")
+            print("Bienvenido el juego va a comenzar")
+            print("La palabra es")
+            read()
+            break
 
 
         elif usuario_uno == 2:
-           # os.system("cls")
+            print(os.system("cls"))
             print("Reglas")
             print("Regla 1: adivinar la palabra")
             print("Regla 2: Tienes un total de 8 vidas")
@@ -42,6 +66,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+   main()
+
 
 
